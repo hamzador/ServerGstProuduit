7191538be3998d00492f078948c51d78209f8403
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2020-01-05 16:36:29 +0100

    firstName LastName and register Service

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,18 +2,28 @@ package com.example.demo.controller;
 
 import java.security.Principal;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.impl.UserService;
 
 @RestController
 //@CrossOrigin("*")
 //@RequestMapping("/api/user")
-public class UserController {
+public class UserController extends CrudController<User, Long> {
 	
 	@RequestMapping("/api/user")
-	
 	public Principal user(Principal user) {
 		return user;
 	}
+	
+	@Autowired
+	private UserService userservice;
+	public void add(@RequestBody User user) {
+		userservice.add(user);
+	}
+	
 }
